3a0fbdd3229d1a708f3bc49e80356b9aaf41cc97
LukeDMountain <121026958+LukeDMountain@users.noreply.github.com>
2025-05-20 16:17:27 -0300

    Atualização dos botões!

--- app/src/main/java/com/example/ecommerceapp/Activity/HeadsetsActivity.java
+++ app/src/main/java/com/example/ecommerceapp/Activity/HeadsetsActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ecommerceapp.Domain.PopularDomain;
 import com.example.ecommerceapp.R;
+
+import java.util.ArrayList;
 
 public class HeadsetsActivity extends AppCompatActivity {
     private RecyclerView recyclerViewHeadsets;
@@ -13,6 +17,12 @@ public class HeadsetsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_headsets); // Referência ao XML headsets.xml
+
+        // Adicionando dados fictícios
+        ArrayList<PopularDomain> items = new ArrayList<>();
+
+        items.add(new PopularDomain("Jogo de Ação Y", "Descrição do produto", "imagem_url", 5, 4.7, 60));
+
 
         recyclerViewHeadsets = findViewById(R.id.headsetsRecyclerView);
         recyclerViewHeadsets.setLayoutManager(new LinearLayoutManager(this));
